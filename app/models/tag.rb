177bc22a7a7bd5b081bede3675eba1923aae5dc2
Sflaton <suzanne.flaton@gmail.com>
2016-05-29 15:47:13 +0200

    working tags with partials

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :poems
   has_and_belongs_to_many :short_stories
+  has_and_belongs_to_many :authors
 end
---
+++ test/models/tag_author_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TagAuthorTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
